2f89c0e66854e599e305bb8b07ecd7deb8fbe581
Rafael Serrano Aracil <rsa26@alu.ua.es>
2020-06-15 15:55:48 +0200

    Pruebas menú

---
+++ gol.php
@@ -0,0 +1,2 @@
+<h1>GOL.PHP</h1>
+<h2>amoave</h2>
---
+++ master.php
@@ -0,0 +1,5 @@
+<body>
+<div><?php include('menu.php');?></div>
+<div><?php include($page_content);?></div>
+<div><?php include('footer.php');?></div>
+</body>
